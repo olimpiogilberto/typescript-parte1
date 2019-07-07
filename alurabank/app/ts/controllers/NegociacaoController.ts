0a3edd236acf3f36ff8b6789254f480e148a3da4
c129427 <olimpiogilberto@gmail.com>
2019-07-07 17:39:49 -0300

    Fim TypeScript 1
    funcionando com jquery

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,28 +1,32 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
 
-
-    constructor(){
-
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+    constructor() {
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
-    adiciona(event) {
+    adiciona(event: Event) {
 
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value
+            new Date(this._inputData.val().replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
         );
 
-        console.log(negociacao);
-    }
+        this._negociacoes.adiciona(negociacao);
 
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
 }
--- alurabank/app/ts/models/Negociacao.ts
+++ alurabank/app/ts/models/Negociacao.ts
@@ -1,29 +1,24 @@
-class Negociacao{
+class Negociacao {
+    
+    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
 
-   private _data;
-   private _quantidade;
-   private _valor;
+    get data() {
 
-    constructor(data,quantidade,valor){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-
-    get data(){
         return this._data;
     }
-    
-    get quantidade(){
+
+    get quantidade() {
+
         return this._quantidade;
     }
-    
-    get valor(){
+
+    get valor() {
+
         return this._valor;
     }
-    
-    get volume(){
+
+    get volume() {
+
         return this._quantidade * this._valor;
     }
 }
-
